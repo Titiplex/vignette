72146459a818c482f40bbe15151c5e65b9c4e7ca
Titiplex <107863779+Titiplex@users.noreply.github.com>
2026-03-11 16:49:28 -0400

    API config update

--- src/main/java/org/titiplex/config/OpenApiConfig.java
+++ src/main/java/org/titiplex/config/OpenApiConfig.java
@@ -2,7 +2,9 @@ package org.titiplex.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
@@ -19,12 +21,22 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("Vignette API")
                         .description("Automated OpenAPI documentation, automatically generated for Java endpoints.")
-                        .version("v1"))
-                .components(new Components().addSecuritySchemes(bearerScheme,
-                        new SecurityScheme()
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")))
+                        .version("v1")
+                        .contact(new Contact()
+                                .name("Titouan Johanny")
+                                .email("johannytitouan@gmail.com")
+                                .url("https://github.com/Titiplex/vignette")
+                        )
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")))
+                .components(new Components()
+                        .addSecuritySchemes(bearerScheme,
+                                new SecurityScheme()
+                                        .name(bearerScheme)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
                 .addSecurityItem(new SecurityRequirement().addList(bearerScheme));
     }
 

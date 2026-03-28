8eb3fa0b0c4161819f8ce50c41c752d06745c099
Titiplex <107863779+Titiplex@users.noreply.github.com>
2026-03-28 15:56:06 -0400

    correction for public operation desc

--- src/main/java/org/titiplex/api/security/PublicOperation.java
+++ src/main/java/org/titiplex/api/security/PublicOperation.java
@@ -1,13 +1,10 @@
 package org.titiplex.api.security;
 
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-
 import java.lang.annotation.*;
 
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@SecurityRequirement(name = "bearerAuth")
 @ApiAccess(level = ApiAccessLevel.PUBLIC, rule = "Public Endpoint, no authentication required.")
 public @interface PublicOperation {
 }

0024e2c3d1ad9a3753d5f190b06c3d116f44b14d
Titiplex <107863779+Titiplex@users.noreply.github.com>
2026-04-07 19:50:43 -0400

    cleaning

--- src/main/java/org/titiplex/handler/exception/ThumbnailNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package org.titiplex.handler.exception;
-
-public class ThumbnailNotFoundException extends RuntimeException {
-
-    public ThumbnailNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
